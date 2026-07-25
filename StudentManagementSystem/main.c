51685d391d0d06c66e14674ef97c369fb7055e80
Subij Dhital <subij.dhital@gmail.com>
2026-07-25 20:53:29 +0545

    Initial Commit

---
+++ StudentManagementSystem/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  StudentManagementSystem
+//
+//  Created by Subij Dhital on 25/07/2026.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
